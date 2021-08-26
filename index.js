3cb73672897c1d8c73da6a46fa9d58f95a6bf88d
Mobeen Abdullah <mobeenabdullah@gmail.com>
2021-08-26 15:27:54 +0500

    Searching the API on input change and adding delay in the search input

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
-const fetchData = async () => {
+const fetchData = async (searchTerm) => {
     const response = await axios.get('https://www.omdbapi.com/', {
         params: {
             apikey: 'd5bf54f3',
-            i: 'tt0468569'
+            s: searchTerm
         }
     });
 
     console.log(response.data);
 };
 
-fetchData();
+const input = document.querySelector('input');
+let timeoutId;
+const onInput = e => {
+    if(timeoutId) {
+        clearTimeout(timeoutId);
+    }
+    timeoutId = setTimeout(() => {
+        fetchData(e.target.value);
+    }, 1000);
+}
+input.addEventListener('input', onInput);
