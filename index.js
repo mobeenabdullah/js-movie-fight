702d5db34454d0cbcf08eaaea7fbac72c34ba2bf
Mobeen Abdullah <mobeenabdullah@gmail.com>
2021-08-27 03:30:53 +0500

    Moving HTML generation and repairing references

--- index.js
+++ index.js
@@ -13,20 +13,37 @@ const fetchData = async (searchTerm) => {
     return response.data.Search
 };
 
+const root = document.querySelector('.autocomplete');
+
+root.innerHTML = `
+    <label><b>Search for a Movie</b></label>
+    <input type="text" class="input">
+    <div class="dropdown">
+        <div class="dropdown-menu">
+            <div class="dropdown-content results"></div>
+        </div>
+    </div>
+`;
+
 const input = document.querySelector('input');
+const dropdown = document.querySelector('.dropdown');
+const resultsWrapper = document.querySelector('.results');
 
 const onInput = async e => {
     const movies = await fetchData(e.target.value);
     
+    resultsWrapper.innerHTML = '';
+    dropdown.classList.add('is-active');
     for (let movie of movies) {
-        const div = document.createElement('div');
+        const movieOption = document.createElement('a');
 
-        div.innerHTML = `
+        movieOption.classList.add('dropdown-item')
+        movieOption.innerHTML = `
             <img src="${movie.Poster}" />
-            <h1>${movie.Title}</h1>
+            ${movie.Title}
         `;
 
-        document.querySelector('#target').appendChild(div);
+        resultsWrapper.appendChild(movieOption);
     }
 
 };
