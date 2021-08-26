21c672c9f84b87351699365cab18a989bd4b6a4f
Mobeen Abdullah <mobeenabdullah@gmail.com>
2021-08-26 14:15:13 +0500

    Fetching movies data in search endpoint

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('Hi there!');
+const fetchData = async () => {
+    const response = await axios.get('https://www.omdbapi.com/', {
+        params: {
+            apikey: 'd5bf54f3',
+            s: 'dark'
+        }
+    });
+
+    console.log(response.data);
+};
+
+fetchData();
